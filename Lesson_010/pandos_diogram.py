216472c2962ff0741fdbe7c1d658e6cb4cf9f32d
Sergio8800 <wasp8800@gmail.com>
2023-06-23 21:16:26 +0200

    Lesson109

---
+++ Lesson_010/pandos_diogram.py
@@ -0,0 +1,25 @@
+# #10.1[44]: Дан код, генерирующий DataFrame, которая состоит всего из 1 столбца. Ваша задача перевести его в one hot вид.
+# Статья про one hot вид https://colab.research.google.com/drive/1qKamnDiRmpRZkpiqWPkunBdAhmzhMcGz?usp=sharing
+#     Сделайте one hot вид с использованием get_dummies
+#     Сделайте one hot вид с использованием изученных методов группировки данных. Обратите внимание, что Nan это не ноль.
+
+import random
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+df = pd.read_csv("Pokemon.csv")
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+# data_ar = ['robot', 'human', np.nan]
+print(df)
+df_2 = pd.get_dummies(data)
+print(df_2)
+onehot_encoder = OneHotEncoder(sparse_output=False)
+new_data = onehot_encoder.fit_transform(df_2)
+print(new_data)
+
+
+
